c0a3343f80a7265b20e4156af575c742961a046d
João Fernandes <jbrunomf@outlook.com>
2020-10-28 13:57:27 -0400

    App retornando a condição climatica do local

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+import requests
+import json
+import pprint
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+accuweatherAPIkey = 'Zop2rFzP4JFkQjfKBqQg42oqIEiA5j7j'
+# locationKey = "44157"
+r = requests.get('http://www.geoplugin.net/json.gp')
+
+if r.status_code == 200:
+    location = json.loads(r.text)
+    longitude = location['geoplugin_longitude']
+    latitude = location['geoplugin_latitude']
+    # print(f'Latitude: {latitude} Longitude: {longitude}')
+    locationAPiUrl = "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey" \
+                     "="+accuweatherAPIkey+"&q="+latitude+"%2C"+longitude+"&language=pt-br"
+
+    r2 = requests.get(locationAPiUrl)
+    if r.status_code != 200:
+        print('Não foi possível obter o código do local.')
+    else:
+        response = json.loads(r2.text)
+        state = response['AdministrativeArea']['LocalizedName']
+        country = response['Country']['LocalizedName']
+        city = response['SupplementalAdminAreas'][0]['LocalizedName']
+        locationKey = response['Key']
+        print(f'Cidade: {city} Estado: {state} País: {country} Código do local (Accuweather): {locationKey}')
+
+        currentConditionsAPIUrl = "http://dataservice.accuweather.com/currentconditions/v1/"+locationKey+"?apikey" \
+                                  "="+accuweatherAPIkey+"&language=pt-br"
+        r3 = requests.get(currentConditionsAPIUrl)
+        if r3.status_code == 200:
+            currentConditionsResponse = json.loads(r3.text)
+            weatherText = currentConditionsResponse[0]['WeatherText']
+            temperature = currentConditionsResponse[0]['Temperature']['Metric']['Value']
+            print(f'A temperatura atual é: {temperature} ºC, Condição climática: {weatherText}')
+        else:
+            print(r3.status_code)
+            print('Não foi possível obter o código do local.')
+else:
+    print('Não foi possível obter a localização.')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
