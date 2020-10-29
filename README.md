76d3a5330a3edc02886711f2ff5ae4f02a04edc7
João Fernandes <jbrunomf@outlook.com>
2020-10-29 10:46:20 -0400

    Refatorando, separando por funções.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 #### Utilizando:
 * API Accuweather para identificar o local. Location API  - https://developer.accuweather.com/accuweather-locations-api/apis
 * API da Accuweather para identificar a previsão do tempo no local. Current Conditions API - https://developer.accuweather.com/accuweather-current-conditions-api/apis
-* GeoPlugin para obter latitude e longitude. https://www.geoplugin.com/webservices/json
+* GeoPlugin para obter latitude e longitude a partir do IP. https://www.geoplugin.com/webservices/json
 
 
 ##### Screenshot:
--- main.py
+++ main.py
@@ -1,42 +1,63 @@
 import requests
 import json
-import pprint
 
 accuweatherAPIkey = 'Zop2rFzP4JFkQjfKBqQg42oqIEiA5j7j'
-# locationKey = "44157"
-r = requests.get('http://www.geoplugin.net/json.gp')
 
-if r.status_code == 200:
-    location = json.loads(r.text)
-    longitude = location['geoplugin_longitude']
-    latitude = location['geoplugin_latitude']
-    # print(f'Latitude: {latitude} Longitude: {longitude}')
-    locationAPiUrl = "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey" \
-                     "="+accuweatherAPIkey+"&q="+latitude+"%2C"+longitude+"&language=pt-br"
 
-    r2 = requests.get(locationAPiUrl)
+def get_coordinates():
+    r = requests.get('http://www.geoplugin.net/json.gp')
+    if r.status_code == 200:
+        location = json.loads(r.text)
+        coordinates = {'lat': location['geoplugin_latitude'], 'long': location['geoplugin_longitude']}
+        # longitude = location['geoplugin_longitude']
+        # latitude = location['geoplugin_latitude']
+        # print(f'Latitude: {latitude} Longitude: {longitude}')
+        return coordinates
+    else:
+        return f'Erro ao obter coordenadas.'
+
+
+def get_location_code(lat, long):
+    location_api_url = "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey" \
+                       "=" + accuweatherAPIkey + "&q=" + lat + "%2C" + long + "&language=pt-br"
+    r = requests.get(location_api_url)
+    # Se a requisição der erro:
     if r.status_code != 200:
         print('Não foi possível obter o código do local.')
+    # Se obter resposta com sucesso:
     else:
-        response = json.loads(r2.text)
-        state = response['AdministrativeArea']['LocalizedName']
-        country = response['Country']['LocalizedName']
-        city = response['SupplementalAdminAreas'][0]['LocalizedName']
-        locationKey = response['Key']
-        print(f'Cidade: {city} Estado: {state} País: {country} Código do local (Accuweather): {locationKey}')
+        response = json.loads(r.text)
+        localInfo = {'city': response['SupplementalAdminAreas'][0]['LocalizedName'],
+                     'state': response['AdministrativeArea']['LocalizedName'],
+                     'country': response['Country']['LocalizedName'],
+                     'locationKey': response['Key']}
+        return localInfo
 
-        currentConditionsAPIUrl = "http://dataservice.accuweather.com/currentconditions/v1/"+locationKey+"?apikey" \
-                                  "="+accuweatherAPIkey+"&language=pt-br"
-        r3 = requests.get(currentConditionsAPIUrl)
-        if r3.status_code == 200:
-            currentConditionsResponse = json.loads(r3.text)
-            weatherText = currentConditionsResponse[0]['WeatherText']
-            temperature = currentConditionsResponse[0]['Temperature']['Metric']['Value']
-            print(f'A temperatura atual é: {temperature} ºC, Condição climática: {weatherText}')
-        else:
-            print(r3.status_code)
-            print('Não foi possível obter o código do local.')
-else:
-    print('Não foi possível obter a localização.')
+
+def get_weather_now(local_code, local_name):
+    currentConditionsAPIUrl = "http://dataservice.accuweather.com/currentconditions/v1/" + local_code + "?apikey" \
+                              "=" + accuweatherAPIkey + "&language=pt-br"
+    r = requests.get(currentConditionsAPIUrl)
+    if r.status_code == 200:
+        currentConditionsResponse = json.loads(r.text)
+        weather_info = {'weatherText': currentConditionsResponse[0]['WeatherText'],
+                        'temperature': currentConditionsResponse[0]['Temperature']['Metric']['Value'],
+                        'localName': local_name}
+        # print(f'A temperatura atual é: {weather_info["temperature"]} ºC, Condição climática: {weather_info["weatherText"]}')
+        return weather_info
+    else:
+        print(r.status_code)
+        print('Não foi possível obter o código do local.')
+
+
+# Beggins
+
+coordinates = get_coordinates()
+local = get_location_code(coordinates['lat'], coordinates['long'])
+weather_conditions = get_weather_now(local['locationKey'], local['city'])
+
+print(f'Clima atual em: {weather_conditions["localName"]}')
+print(f'Temperatura {weather_conditions["temperature"]} ºC')
+print(f'Condição climática: {weather_conditions["weatherText"]}')
 
 
